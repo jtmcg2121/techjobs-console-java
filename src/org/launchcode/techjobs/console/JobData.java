f3080c5cc9f16f736e0d7db3109983dac9b71b31
jtmcg2121 <jtmcg2121@gmail.com>
2021-05-20 19:30:54 -0500

    to lower case

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by LaunchCode
@@ -81,6 +82,27 @@ public class JobData {
             }
         }
 
+        return jobs;
+    }
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row: allJobs){
+
+            for (String jobKey: row.keySet()){
+                String aValue = row.get(jobKey);
+
+                aValue = aValue.toLowerCase();
+                value = value.toLowerCase();
+
+            if (aValue.contains(value)) {
+                jobs.add(row);
+            }
+
+            }
+        }
         return jobs;
     }
 
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.console;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
 
 /**
  * Created by LaunchCode
@@ -61,7 +64,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +113,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() > 0) {
 
-        System.out.println("printJobs is not implemented yet");
+            for (int i = 0; i < someJobs.size(); i++) {
+
+                for (Map.Entry<String, String> job : someJobs.get(i).entrySet()) {
+                    System.out.println(job.getKey() + ":" + "(" + job.getValue() + ")");
+                }
+                System.out.println("*****");
+            }
+        } else{
+            System.out.println("No results found, please try a different search.");
+        }
     }
 }
